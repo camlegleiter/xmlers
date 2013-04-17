f027f1ac15afe6bc487a3a6d3c748e6335f80d81
Cameron Legleiter <camlegleiter@gmail.com>
2013-04-17 04:37:20 -0500

    Differentiation code between the three submit buttons on the index.jsp page

--- src/Index.java
+++ src/Index.java
@@ -6,6 +6,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import email.EmailParticipants;
+
 /**
  * Servlet implementation class Index
  */
@@ -45,6 +47,23 @@ public class Index extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		String viewRecords = request.getParameter("viewRecords");
+		String editForm = request.getParameter("editForm");
+		String reemailParticipants = request.getParameter("reemailParticipants");
+		
+		String formID = request.getParameter("formid");
+		
+		
+		
+		if (null != viewRecords) {
+			response.sendRedirect(request.getContextPath() + "/app/view.jsp?formid=" + formID);
+		} else if (null != editForm) {
+			response.sendRedirect(request.getContextPath() + "/app/edit.jsp?formid=" + formID);
+		} else if (null != reemailParticipants) {
+			// This should only be allowed to execute once every X number of hours/days/etc.
+			EmailParticipants.reemailParticipants(formID);
+		}
+
 	}
 
 	/**

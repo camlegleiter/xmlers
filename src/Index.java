63a9e3f983d622a4fd3a065059dc1a9c7e35ec07
Cameron Legleiter <camlegleiter@gmail.com>
2013-04-18 02:26:12 -0500

    Moved logout functionality to new servlet

--- src/Index.java
+++ src/Index.java
@@ -26,20 +26,7 @@ public class Index extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// Check if the user clicked the "logout" button
-		String logout = request.getParameter("logout");
-		if (null != logout && logout.equals("logout")) {
-			request.getSession().invalidate();
-			deleteRememberMeCookie(request);
-			
-			// Doubly ensures that the headers prevent users from going back after logging out
-			// "Doubly" in the sense that the LoginFilter also sets the headers as well.
-			response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
-			response.setHeader("Pragma", "no-cache");
-			response.setDateHeader("Expires", 0);
-			
-			response.sendRedirect(request.getContextPath() + "/login.jsp");
-		}
+
 	}
 
 	/**
@@ -64,26 +51,5 @@ public class Index extends HttpServlet {
 			EmailParticipants.reemailParticipants(formID);
 		}
 
-	}
-
-	/**
-	 * Used to specifically remove the "Remember Me" cookie that may be created on user login.
-	 * 
-	 * Calling cookie.setMaxAge(0) will inform the client/server to destroy the cookie.
-	 * 
-	 * @param request
-	 */
-	private void deleteRememberMeCookie(HttpServletRequest request) {
-		Cookie[] cookies = request.getCookies(); // request is an instance of type HttpServletRequest
-
-		if (null != cookies) {
-			for (int i = 0; i < cookies.length; i++) {
-				Cookie c = cookies[i];
-				if (c.getName().equals("userid")) {
-					c.setMaxAge(0);
-					return;
-				}
-			}
-		}
 	}
 }

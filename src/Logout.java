105cbfbf99c6d171681af77444fdea369e8cfc3d
Cameron Legleiter <camlegleiter@gmail.com>
2013-04-19 12:55:01 -0500

    Changed mapping url to match web.xml mapping

--- src/Logout.java
+++ src/Logout.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Logout
  */
-@WebServlet("/Logout")
+@WebServlet("/app/logout")
 public class Logout extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        

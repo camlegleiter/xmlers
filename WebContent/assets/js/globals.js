2fa5313fd287dbc9e42ed9986890a82fb0408d32
Cameron Legleiter <camlegleiter@gmail.com>
2013-09-18 19:32:40 -0500

    Added new template regex for parsing templates in JSP

--- WebContent/assets/js/globals.js
+++ WebContent/assets/js/globals.js
@@ -1,3 +1,9 @@
 TaskManager = new Backbone.Marionette.Application();
 TaskManager.Models = {};
 TaskManager.Collections = {};
+
+_.templateSettings = {
+    interpolate: /\<\@\=(.+?)\@\>/gim,
+    evaluate: /\<\@(.+?)\@\>/gim,
+    escape: /\<\@\-(.+?)\@\>/gim
+};

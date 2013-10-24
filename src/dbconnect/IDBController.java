cf9fb165ff515a1e974a2cd5811fde609cdd493a
Cameron Legleiter <camlegleiter@gmail.com>
2013-10-23 19:00:56 -0500

    Added JavaDoc to IDBController interface

--- src/dbconnect/IDBController.java
+++ src/dbconnect/IDBController.java
@@ -5,59 +5,96 @@ import java.util.List;
 import form.Form;
 import form.User;
 
-public interface IDBController  
-{	
+public interface IDBController {
 	/**
 	 * Returns <code>true</code> if the form with the given ID exists.
 	 * 
 	 * @param formId
-	 * 				The ID of the form to check for existence.
+	 *            The ID of the form to check for existence.
 	 * @return
 	 */
 	public boolean formExists(String formId);
-	
+
 	/**
 	 * Returns <code>true</code> if the user with the given ID exists.
 	 * 
 	 * @param formId
-	 * 				The ID of the form to check for existence.
+	 *            The ID of the form to check for existence.
 	 * @return
 	 */
 	public boolean userExists(String userId);
-	
+
 	/**
 	 * Checks if the given form exists in the database. If it does, the form
 	 * information is updated (UPsert). Otherwise, the form is inserted (upSERT)
 	 * as a new form.
 	 * 
 	 * @param f
-	 * 				the Form object to upsert into the database.
-	 * @return
-	 * 				<code>true</code> if the form was correctly upserted
+	 *            the Form object to upsert into the database.
+	 * @return <code>true</code> if the form was correctly upserted
 	 */
 	public boolean upsertForm(Form form);
-	
+
 	/**
 	 * Checks if the given user exists in the database. If it does, the user
 	 * information is updated (UPsert). Otherwise, the user is inserted (upSERT)
 	 * as a new form.
 	 * 
 	 * @param f
-	 * 				the User object to upsert into the database.
-	 * @return
-	 * 				<code>true</code> if the form was correctly upserted
+	 *            the User object to upsert into the database.
+	 * @return <code>true</code> if the form was correctly upserted
 	 */
 	public boolean upsertUser(User user);
-	
+
+	/**
+	 * Retrieves a Form with the given ID.
+	 * 
+	 * @param formId
+	 * @return The corresponding Form, or <code>null</code> if the given formId
+	 *         is null or has length == 0, or if there is no Form that exists
+	 *         that matches the formId
+	 */
 	public Form fetchForm(String formId);
-	
+
+	/**
+	 * Retrieves a User with the given ID.
+	 * 
+	 * @param formId
+	 * @return The corresponding User, or <code>null</code> if the given userId
+	 *         is null or has length == 0, or if there is no User that exists
+	 *         that matches the userId
+	 */
 	public User fetchUser(String userId);
-	
+
+	/**
+	 * Deletes a Form with the given ID.
+	 * 
+	 * @param formId
+	 * @return <code>true</code> if the Form matching the given ID was deleted.
+	 */
 	public boolean deleteForm(String formId);
-	
+
+	/**
+	 * Deletes a User with the given ID.
+	 * 
+	 * @param userId
+	 * @return <code>true</code> if the User matching the given ID was deleted.
+	 */
 	public boolean deleteUser(String userId);
 
-	public List<Form> getOwnerForms(String userID);
+	/**
+	 * Retrieves all of the Forms where the owner matches the given userId.
+	 * 
+	 * @param userId
+	 * @return a List of all Forms that the given userId is the owner of.
+	 */
+	public List<Form> getOwnerForms(String userId);
 
-	public List<Form> getParticipantForms(String userID);	
+	/**
+	 * Retrieves all of the Forms the given userId is a participant of.
+	 * 
+	 * @param userId
+	 * @return a List of all Forms that the given userId is a participant of.
+	 */
+	public List<Form> getParticipantForms(String userId);
 }

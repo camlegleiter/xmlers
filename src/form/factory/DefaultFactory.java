74f91eed80fa0d1ff9c26aea985d280149bb739e
Cameron Legleiter <camlegleiter@gmail.com>
2013-10-28 09:39:37 -0500

    Refactored factory package to implement a Factory-type design pattern

--- src/form/factory/DefaultFactory.java
+++ src/form/factory/DefaultFactory.java
@@ -1,8 +1,15 @@
 package form.factory;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import dbconnect.DBManager;
 
 import form.Form;
 import form.questions.CheckQuestion;
@@ -12,98 +19,136 @@ import form.questions.SelectQuestion;
 import form.questions.TextQuestion;
 
 public class DefaultFactory extends FormFactory {
-	
+
+	private static Map<String, IQuestionBuilder> builderType = new HashMap<String, IQuestionBuilder>();
+	static {
+		builderType.put("TEXTBOX", new TextboxQuestionBuilder());
+		builderType.put("CHECKBOX", new CheckboxQuestionBuilder());
+		builderType.put("RADIO", new RadioQuestionBuilder());
+		builderType.put("SELECT", new SelectQuestionBuilder());
+	}
+
 	@Override
-	public Form BuildForm(JSONObject src) throws JSONException {
+	public Form buildForm(JSONObject jsonObject) throws JSONException {
 		Form f = new Form();
-		f.setFormId(src.getInt("formID"));
-		f.setDescription(src.getString("formDescription"));
-		f.setTitle(src.getString("formName"));
-		f.setOwnerId(src.getInt("formOwner"));
+		f.setFormId(jsonObject.getInt("formID"));
+		f.setDescription(jsonObject.getString("formDescription"));
+		f.setTitle(jsonObject.getString("formName"));
+		f.setOwnerId(jsonObject.getInt("formOwner"));
+
+		f.canParticipantsSeeAll(jsonObject.getBoolean("participantsCanSeeAll"));
+		f.canParticipantsEditResponse(jsonObject
+				.getBoolean("participantsCanEditResponse"));
+		f.isParticipantResponseRequired(jsonObject
+				.getBoolean("participantResponseIsRequired"));
 		
-		f.canParticipantsSeeAll(src.getBoolean("participantsCanSeeAll"));
-		f.canParticipantsEditResponse(src.getBoolean("participantsCanEditResponse"));
-		f.isParticipantResponseRequired(src.getBoolean("participantResponseIsRequired"));
-		
-		JSONArray questions = src.getJSONArray("formQuestions");
-		for(int i = 0; i < questions.length(); ++i)
-		{	
-			JSONObject currentObj = questions.getJSONObject(i);
-			f.add(BuildQuestion(currentObj));
-		}
-		
+		JSONArray participants = jsonObject.getJSONArray("formParticipants");
+		for (int i = 0; i < participants.length(); ++i)
+			f.addParticipant(DBManager.getInstance().fetchUserByEmail(participants.getString(i)));
+
+		JSONArray questions = jsonObject.getJSONArray("formQuestions");
+		for (int i = 0; i < questions.length(); ++i)
+			f.add(buildQuestion(questions.getJSONObject(i), i));
+
 		return f;
 	}
 
 	@Override
-	public Question<?> BuildQuestion(JSONObject src) throws JSONException {
-		Question<?> retval;
-		String type = src.getString("type");
-		
-		switch(type)
-		{
-		case "Textbox":
-			retval = buildStringQuestion(src);
-			break;
-		case "Checkbox":
-			retval = buildCheckQuestion(src);
-			break;
-		case "Radio":
-			retval = buildRadioQuestion(src);
-			break;
-		case "Select":
-			retval = buildSelectQuestion(src);
-			break;
-		default:
-			throw new IllegalArgumentException("When building Question: Unrecognized type of JSONObject");
-		}
-		
-		return retval;
-	}
-	
-	private static TextQuestion buildStringQuestion(JSONObject src) throws JSONException
-	{
-		TextQuestion tq = new TextQuestion();
-		tq.setMaxLength(src.getInt("maxLength"));
-		tq.setPrompt(src.getString("prompt"));
-		
-		return tq;
-	}
-	
-	/**
-	 * Assuming an appropriate JSONObject is given to it, a CheckQuestion is
-	 * built that matches that JSONObjecct.
-	 * @param src
-	 * @return
-	 */
-	private static CheckQuestion buildCheckQuestion(JSONObject src) throws JSONException
-	{
-		return buildCheckHelper(src, "checkboxes", "label");
+	public Question<?> buildQuestion(JSONObject jsonObject, int position)
+			throws JSONException {
+		String type = jsonObject.getString("type");
+		IQuestionBuilder builder = builderType.get(type.toUpperCase());
+		if (builder == null)
+			throw new IllegalArgumentException(
+					"When building Question: Unrecognized question type.");
+		return builder.buildQuestion(jsonObject, position);
 	}
 
-	private static RadioQuestion buildRadioQuestion(JSONObject src) throws JSONException
-	{
-		return new RadioQuestion(buildCheckHelper(src, "radios", "label"));
+	private interface IQuestionBuilder {
+		/**
+		 * Creates a Question object from the given JSONObject at a specific
+		 * position.
+		 * 
+		 * @param jsonObject
+		 * @param position
+		 * @return
+		 * @throws JSONException
+		 */
+		public Question<?> buildQuestion(JSONObject jsonObject, int position)
+				throws JSONException;
 	}
-	
-	private static SelectQuestion buildSelectQuestion(JSONObject src) throws JSONException
-	{
-		return new SelectQuestion(buildCheckHelper(src, "options", "value"));
-	}
-	
-	private static CheckQuestion buildCheckHelper(JSONObject src, String kind, String inner) throws JSONException
-	{
-		JSONArray givenOptions = src.getJSONArray(kind);
-		
-		String[] parsedOptions = new String[givenOptions.length()];
-		for(int i = 0; i < parsedOptions.length; i++)
-		{
-			JSONObject jo = givenOptions.getJSONObject(i);
-			parsedOptions[i] = jo.getString(inner);
+
+	private static class TextboxQuestionBuilder implements IQuestionBuilder {
+		@Override
+		public Question<?> buildQuestion(JSONObject jsonObject, int position)
+				throws JSONException {
+			TextQuestion question = new TextQuestion();
+			question.setPrompt(jsonObject.getString("prompt"));
+			question.setMaxLength(jsonObject.getInt("maxLength"));
+			question.setPosition(position);
+			return question;
 		}
-		
-		CheckQuestion cq = new CheckQuestion(parsedOptions);
-		cq.setPrompt(src.getString("prompt"));
-		return cq;
+	}
+
+	private static class CheckboxQuestionBuilder implements IQuestionBuilder {
+		@Override
+		public Question<?> buildQuestion(JSONObject jsonObject, int position)
+				throws JSONException {
+			CheckQuestion question = new CheckQuestion(buildOptionsArray(
+					jsonObject, "checkboxes", "label"));
+			question.setPrompt(jsonObject.getString("prompt"));
+			question.setPosition(position);
+
+			return question;
+		}
+
+		/**
+		 * Creates a List of different options available for a question from the
+		 * given JSONObject.
+		 * 
+		 * @param jsonObject
+		 * @param arrayName
+		 *            - the name of the array in the JSONObject to get the
+		 *            different values from.
+		 * @param labelName
+		 *            - the key name of each object in the array.
+		 * @return
+		 * @throws JSONException
+		 */
+		protected List<String> buildOptionsArray(JSONObject jsonObject,
+				String arrayName, String labelName) throws JSONException {
+			JSONArray options = jsonObject.getJSONArray(arrayName);
+			List<String> parsedOptions = new ArrayList<String>(options.length());
+			for (int i = 0; i < options.length(); i++) {
+				JSONObject jo = options.getJSONObject(i);
+				parsedOptions.add(jo.getString(labelName));
+			}
+			return parsedOptions;
+		}
+	}
+
+	private static class RadioQuestionBuilder extends CheckboxQuestionBuilder {
+		@Override
+		public Question<?> buildQuestion(JSONObject jsonObject, int position)
+				throws JSONException {
+			RadioQuestion question = new RadioQuestion(buildOptionsArray(
+					jsonObject, "radios", "label"));
+			question.setPrompt(jsonObject.getString("prompt"));
+			question.setPosition(position);
+			return question;
+		}
+	}
+
+	private static class SelectQuestionBuilder extends CheckboxQuestionBuilder {
+		@Override
+		public Question<?> buildQuestion(JSONObject jsonObject, int position)
+				throws JSONException {
+			SelectQuestion question = new SelectQuestion(buildOptionsArray(
+					jsonObject, "options", "value"));
+			question.setPrompt(jsonObject.getString("prompt"));
+			question.setMulti(jsonObject.getBoolean("isMulti"));
+			question.setPosition(position);
+			return question;
+		}
 	}
 }

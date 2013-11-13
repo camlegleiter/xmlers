68ede4f49ed618f4dd3d8f6cc3e4170896f818e6
Martin Strobel <mstrobel@iastate.edu>
2013-11-13 16:51:41 -0600

    Initial teardown of previous multi-file structure, replaced with two monolithic files.

--- src/dbconnect/xml/dao/Forms.java
+++ src/dbconnect/xml/dao/Forms.java
@@ -12,22 +12,23 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
 
 /**
- * <p>Java class for ComplexQuestion complex type.
+ * <p>Java class for anonymous complex type.
  * 
  * <p>The following schema fragment specifies the expected content contained within this class.
  * 
  * <pre>
- * &lt;complexType name="ComplexQuestion">
+ * &lt;complexType>
  *   &lt;complexContent>
- *     &lt;extension base="{http://www.example.org/Form}question">
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
  *       &lt;sequence>
- *         &lt;element name="question" type="{http://www.example.org/Form}question" maxOccurs="unbounded" minOccurs="0"/>
+ *         &lt;element name="form" type="{http://www.example.org/Form}form" maxOccurs="unbounded" minOccurs="0"/>
  *       &lt;/sequence>
- *     &lt;/extension>
+ *     &lt;/restriction>
  *   &lt;/complexContent>
  * &lt;/complexType>
  * </pre>
@@ -35,42 +36,41 @@ import javax.xml.bind.annotation.XmlType;
  * 
  */
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "ComplexQuestion", propOrder = {
-    "question"
+@XmlType(name = "", propOrder = {
+    "form"
 })
-public class ComplexQuestion
-    extends Question
-{
+@XmlRootElement(name = "forms")
+public class Forms {
 
-    protected List<Question> question;
+    protected List<Form> form;
 
     /**
-     * Gets the value of the question property.
+     * Gets the value of the form property.
      * 
      * <p>
      * This accessor method returns a reference to the live list,
      * not a snapshot. Therefore any modification you make to the
      * returned list will be present inside the JAXB object.
-     * This is why there is not a <CODE>set</CODE> method for the question property.
+     * This is why there is not a <CODE>set</CODE> method for the form property.
      * 
      * <p>
      * For example, to add a new item, do as follows:
      * <pre>
-     *    getQuestion().add(newItem);
+     *    getForm().add(newItem);
      * </pre>
      * 
      * 
      * <p>
      * Objects of the following type(s) are allowed in the list
-     * {@link Question }
+     * {@link Form }
      * 
      * 
      */
-    public List<Question> getQuestion() {
-        if (question == null) {
-            question = new ArrayList<Question>();
+    public List<Form> getForm() {
+        if (form == null) {
+            form = new ArrayList<Form>();
         }
-        return this.question;
+        return this.form;
     }
 
 }

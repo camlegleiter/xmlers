c050253d3137290268404039336c541586a1abd7
Cameron Legleiter <camlegleiter@gmail.com>
2013-12-10 10:45:03 -0600

    Added future servlet to handle exporting XML data

---
+++ src/ExportXML.java
@@ -0,0 +1,30 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ExportXML
+ */
+@WebServlet("/ExportXML")
+public class ExportXML extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ExportXML() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+}

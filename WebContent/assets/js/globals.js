37e7c7a09b1dd43579d377fc41007f4ec7fed53d
Cameron Legleiter <camlegleiter@gmail.com>
2013-12-11 13:38:07 -0600

    Slight commenting fix for globals.js

--- WebContent/assets/js/globals.js
+++ WebContent/assets/js/globals.js
@@ -2,9 +2,10 @@ TaskManager = new Backbone.Marionette.Application();
 TaskManager.Models = {};
 TaskManager.Collections = {};
 
-/*
+/**
  * Since the HTML templates are within JSP files, the Underscore template
- * JavaScript tags need to be replaced with <@ @>
+ * JavaScript tags need to be replaced with <@ @> to prevent the JSP from
+ * interpreting the Underscore templates as scriptlets.
  */
 _.templateSettings = {
     interpolate: /\<\@\=(.+?)\@\>/gim,
@@ -12,7 +13,7 @@ _.templateSettings = {
     escape: /\<\@\-(.+?)\@\>/gim
 };
 
-/*
+/**
  * The optional view for CollectionViews or CompositeViews when the view's
  * collection has no items. Initialize the view's message using:
  * 
@@ -23,13 +24,13 @@ _.templateSettings = {
  * and use the 'message' variable name within the HTML template using the
  * Underscore <@= @> evaluate tags:
  * 
- * <script type="text/template">
+ * <script type="text/template" id="empty-view-template">
  *   <@= message @>
  * </script
  */
 TaskManager.EmptyView = Backbone.Marionette.ItemView.extend({
     template: '#empty-view-template',
-    className: 'control-group',
+    className: 'form-group',
 
     initialize: function(options) {
         this.message = options.message;

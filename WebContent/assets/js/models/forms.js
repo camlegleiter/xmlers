144e0b9cb93769f299ab7a3b76259de6ce6ae83e
Cameron Legleiter <camlegleiter@gmail.com>
2013-12-11 13:09:18 -0600

    Added documentation to the Backbone JavaScript models for the front end

--- WebContent/assets/js/models/forms.js
+++ WebContent/assets/js/models/forms.js
@@ -1,27 +1,45 @@
 'use strict';
 
+/**
+ * The Backbone model used in the front end for representing an XML form.
+ */
 TaskManager.Models.Form = Backbone.Model.extend({
-    initialize: function() {
-        this.set('formQuestions', new TaskManager.Collections.Questions(this.get('formQuestions')));
-    },
-    
-    defaults: function() {
-        return {
-            formID: -1,
-            formName: '',
-            formDescription: '',
-            formOwner: -1,
-            formParticipants: [],
-            participantsCanSeeAll: false,
-            participantsCanEditResponse: false,
-            participantResponseIsRequired: true,
-            formQuestions: []
-        };
-    }
+	initialize: function() {
+
+		/*
+		 * Ensures that creating a new form or instantiating a form with
+		 * existing data correctly sets formQuestions as a Backbone collection.
+		 */
+		this.set('formQuestions', new TaskManager.Collections.Questions(this
+				.get('formQuestions')));
+	},
+
+	/*
+	 * The basic model entries. See the form_metadata.json for more information
+	 * on the values used. All values listed here are defaults in the case that
+	 * the key hasn't been set in the form itself.
+	 */
+	defaults: function() {
+		return {
+			formID: -1,
+			formName: '',
+			formDescription: '',
+			formOwner: -1,
+			formParticipants: [],
+			participantsCanSeeAll: false,
+			participantsCanEditResponse: false,
+			participantResponseIsRequired: true,
+			queries: '',
+			formQuestions: []
+		};
+	}
 });
 
+/**
+ * Backbone collection for storing multiple Form models
+ */
 TaskManager.Collections.Forms = Backbone.Collection.extend({
-    model: TaskManager.Models.Form,
-    // Sort models by name
-    comparator: 'formName'
+	model: TaskManager.Models.Form,
+	// Sort models by name when displaying them (mainly used on index page)
+	comparator: 'formName'
 });
--- WebContent/assets/js/models/responses.js
+++ WebContent/assets/js/models/responses.js
@@ -1,121 +1,200 @@
 'use strict';
 
+/**
+ * A Backbone model that uses a modified version of the Form model for storing
+ * user responses. The defaults used represent the base items needed for storing
+ * response information from a particular participant.
+ */
 TaskManager.Models.Response = Backbone.Model.extend({
-    initialize: function() {
-        this.set('responses', new TaskManager.Collections.QuestionResponses(this.get('responses')));
-    },
-    
-    defaults: function() {
-        return {
-            formID: -1,
-            responseOwner: -1,
-            formParticipants: [],
-            responses: []
-        };
-    }
+	initialize: function() {
+
+		/*
+		 * Ensures that creating a new form or instantiating a form with
+		 * existing data correctly sets responses as a Backbone collection.
+		 */
+		this.set('responses',
+				new TaskManager.Collections.QuestionResponses(this
+						.get('responses')));
+	},
+
+	/*
+	 * The basic model entries. See the response_metadata.json for more
+	 * information on the values used. All values listed here are defaults in
+	 * the case that the key hasn't been set in the form itself.
+	 */
+	defaults: function() {
+		return {
+			formID: -1,
+			responseOwner: -1,
+			formParticipants: [],
+			responses: []
+		};
+	}
 });
 
+/**
+ * A Backbone model for storing multiple Response models
+ */
 TaskManager.Collections.Responses = Backbone.Collection.extend({
-    model: TaskManager.Models.Response
+	model: TaskManager.Models.Response
 });
 
-
-/*
+/**
  * An abstract model that acts as a super class for the different response types
  */
 TaskManager.Models.QuestionResponse = Backbone.Model.extend({
-    defaults: {}
+	defaults: {}
 });
 
-/*
- * Stores a collection of responses in the overall model
+/**
+ * A Backbone model for storing individual responses to each question
  */
 TaskManager.Collections.QuestionResponses = Backbone.Collection.extend({
-    model: TaskManager.Models.QuestionResponse
+	model: TaskManager.Models.QuestionResponse
 });
 
-
-/*
- * 
+/**
+ * A Backbone model for storing a participant's responses to a Checkbox field
  */
-TaskManager.Models.CheckboxResponse = TaskManager.Models.QuestionResponse.extend({
-    defaults: function() {
-        return {
-            questionID: -1,
-            type: 'Checkbox',
-            values: new TaskManager.Collections.CheckboxResponses()
-        };
-    }
-});
+TaskManager.Models.CheckboxResponse = TaskManager.Models.QuestionResponse
+		.extend({
 
+			/*
+			 * The basic model entries. See the response_metadata.json for more
+			 * information on the values used. All values listed here are
+			 * defaults in the case that the key hasn't been set in the form
+			 * itself.
+			 */
+			defaults: function() {
+				return {
+					questionID: -1,
+					type: 'Checkbox',
+					values: new TaskManager.Collections.CheckboxResponses()
+				};
+			}
+		});
+
+/**
+ * A Backbone collection of individual CheckboxValues
+ */
 TaskManager.Collections.CheckboxResponses = Backbone.Collection.extend({
-    model: TaskManager.Models.CheckboxValue
+	model: TaskManager.Models.CheckboxValue
 });
 
+/**
+ * A Backbone model for storing one Checkbox input
+ */
 TaskManager.Models.CheckboxValue = Backbone.Model.extend({
-    defaults: {
-        value: ''
-    }
+
+	/*
+	 * The basic model entries. See the response_metadata.json for more
+	 * information on the values used. All values listed here are defaults in
+	 * the case that the key hasn't been set in the form itself.
+	 */
+	defaults: {
+		value: ''
+	}
 });
 
-
-/*
- * 
+/**
+ * A Backbone model for storing a participant's responses to a Radio field
  */
 TaskManager.Models.RadioResponse = TaskManager.Models.QuestionResponse.extend({
-    defaults: function() {
-        return {
-            questionID: -1,
-            type: 'Radio',
-            values: new TaskManager.Collections.RadioResponses()
-        };
-    }
+
+	/*
+	 * The basic model entries. See the response_metadata.json for more
+	 * information on the values used. All values listed here are defaults in
+	 * the case that the key hasn't been set in the form itself.
+	 */
+	defaults: function() {
+		return {
+			questionID: -1,
+			type: 'Radio',
+			values: new TaskManager.Collections.RadioResponses()
+		};
+	}
 });
 
+/**
+ * A Backbone collection of individual RadioValues
+ */
 TaskManager.Collections.RadioResponses = Backbone.Collection.extend({
-    model: TaskManager.Models.RadioValue
+	model: TaskManager.Models.RadioValue
 });
 
+/**
+ * A Backbone model for storing one Radio input
+ */
 TaskManager.Models.RadioValue = Backbone.Model.extend({
-    defaults: {
-        value: ''
-    }
+
+	/*
+	 * The basic model entries. See the response_metadata.json for more
+	 * information on the values used. All values listed here are defaults in
+	 * the case that the key hasn't been set in the form itself.
+	 */
+	defaults: {
+		value: ''
+	}
 });
 
-
-/*
- * 
+/**
+ * A Backbone model for storing a participant's responses to a Text field
  */
 TaskManager.Models.Textbox = TaskManager.Models.QuestionResponse.extend({
-    defaults: function() {
-        return {
-            questionID: -1,
-            type: 'Textbox',
-            value: ''
-        };
-    }
+
+	/*
+	 * The basic model entries. See the response_metadata.json for more
+	 * information on the values used. All values listed here are defaults in
+	 * the case that the key hasn't been set in the form itself.
+	 */
+	defaults: function() {
+		return {
+			questionID: -1,
+			type: 'Textbox',
+			value: ''
+		};
+	}
 });
 
-
-/*
- *
+/**
+ * A Backbone model for storing a participant's responses to a Select field
  */
-TaskManager.Models.SelectResponse = TaskManager.Models.QuestionResponse.extend({
-    defaults: function() {
-        return {
-            questionID: -1,
-            type: 'Select',
-            values: new TaskManager.Collections.SelectResponses()
-        };
-    }
-});
+TaskManager.Models.SelectResponse = TaskManager.Models.QuestionResponse
+		.extend({
 
+			/*
+			 * The basic model entries. See the response_metadata.json for more
+			 * information on the values used. All values listed here are
+			 * defaults in the case that the key hasn't been set in the form
+			 * itself.
+			 */
+			defaults: function() {
+				return {
+					questionID: -1,
+					type: 'Select',
+					values: new TaskManager.Collections.SelectResponses()
+				};
+			}
+		});
+
+/**
+ * A Backbone collection of individual SelectValues
+ */
 TaskManager.Collections.SelectResponses = Backbone.Collection.extend({
-    model: TaskManager.Models.SelectValue
+	model: TaskManager.Models.SelectValue
 });
 
+/**
+ * A Backbone model for storing one option tag of a Select input
+ */
 TaskManager.Models.SelectValue = Backbone.Model.extend({
-    defaults: {
-        value: ''
-    }
+
+	/*
+	 * The basic model entries. See the response_metadata.json for more
+	 * information on the values used. All values listed here are defaults in
+	 * the case that the key hasn't been set in the form itself.
+	 */
+	defaults: {
+		value: ''
+	}
 });

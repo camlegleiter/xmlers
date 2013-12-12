48eb8b03c67f08b2d0c99635fd2c6528f4f5805d
Cameron Legleiter <camlegleiter@gmail.com>
2013-12-11 19:40:44 -0600

    Fixed issue with Select options not saving data or displaying on response page

--- WebContent/assets/js/create/create.form.js
+++ WebContent/assets/js/create/create.form.js
@@ -386,7 +386,7 @@ TaskManager.module("Create", function(Module, App, Backbone, Marionette, $, _) {
         
         onUpdateModel: function() {
             this.model.set({
-                label: this.ui.label.val()
+                value: this.ui.label.val()
             });
         }
     });
@@ -420,7 +420,7 @@ TaskManager.module("Create", function(Module, App, Backbone, Marionette, $, _) {
             }
         },
 
-        // Adds a new checkbox entry to the field
+        // Adds a new select entry to the field
         onAddItem: function() {
             this.addItem(new App.Models.SelectOption());
         },
